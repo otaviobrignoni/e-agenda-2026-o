2e26ffec81cdca20fd6b0f03a72008b586ea2277
otaviobrignoni <otavio.678.b@gmail.com>
2026-06-27 18:38:40 -0300

    corrigo nome errado no banco

--- eAgenda.Database/dbo/Tables/TBItemTarefa.sql
+++ eAgenda.Database/dbo/Tables/TBItemTarefa.sql
@@ -1,16 +1,10 @@
 CREATE TABLE [dbo].[TBItemTarefa] (
     [Id]            UNIQUEIDENTIFIER NOT NULL,
     [Titulo]        NVARCHAR (100)   NOT NULL,
-    [EstaConcluida] BIT              NULL,
-    [TarefaId]      UNIQUEIDENTIFIER NOT NULL
+    [EstaConcluido] BIT              NULL,
+    [TarefaId]      UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_TBItemTarefa] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TBItemTarefa_TBTarefa] FOREIGN KEY ([TarefaId]) REFERENCES [dbo].[TBTarefa] ([Id]) ON DELETE CASCADE
 );
-GO
-
-ALTER TABLE [dbo].[TBItemTarefa]
-    ADD CONSTRAINT [FK_TBItemTarefa_TBTarefa] FOREIGN KEY ([TarefaId]) REFERENCES [dbo].[TBTarefa] ([Id]) ON DELETE CASCADE;
-GO
-
-ALTER TABLE [dbo].[TBItemTarefa]
-    ADD CONSTRAINT [PK_TBItemTarefa] PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
