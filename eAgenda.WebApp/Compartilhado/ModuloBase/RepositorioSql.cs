4376e3f471abe58042858885e73a567a73af61fd
otaviobrignoni <otavio.678.b@gmail.com>
2026-06-28 15:43:54 -0300

    adiciona métodos Query e QuerySingle genéricos no repositorio sql

--- eAgenda.WebApp/Compartilhado/ModuloBase/RepositorioSql.cs
+++ eAgenda.WebApp/Compartilhado/ModuloBase/RepositorioSql.cs
@@ -31,6 +31,12 @@ public abstract class RepositorioSql<TRegistro, TRow>(ISqlConnectionFactory conn
         return conexao.Query<TRow>(sqlQuery).Select(Mapear).Where(filtro ?? (_ => true))!;
     }
 
+    protected IEnumerable<T> Query<T>(string sqlQuery, Guid id = default)
+    {
+        using SqlConnection conexao = connectionFactory.CreateConnection();
+        conexao.Open();
+        return conexao.Query<T>(sqlQuery, new { Id = id }).ToList(); ;
+    }
 
     protected TRegistro? QuerySingle(string sqlQuery, Guid id)
     {
@@ -43,6 +49,13 @@ public abstract class RepositorioSql<TRegistro, TRow>(ISqlConnectionFactory conn
             return null;
 
         return Mapear(row);
+    }
+
+    protected T QuerySingle<T>(string sqlQuery, Guid id)
+    {
+        using SqlConnection conexao = connectionFactory.CreateConnection();
+        conexao.Open();
+        return conexao.QuerySingle<T>(sqlQuery, new { Id = id });
     }
 
     private TRegistro Mapear(TRow row)
