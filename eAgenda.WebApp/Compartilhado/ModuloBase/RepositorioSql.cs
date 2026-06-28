314a1fa90df7aa51b2d24649ec973cd4efa3cb6e
otaviobrignoni <otavio.678.b@gmail.com>
2026-06-28 17:20:31 -0300

    adiciona overload com objeto parametros no repositorio sql

--- eAgenda.WebApp/Compartilhado/ModuloBase/RepositorioSql.cs
+++ eAgenda.WebApp/Compartilhado/ModuloBase/RepositorioSql.cs
@@ -23,6 +23,14 @@ public abstract class RepositorioSql<TRegistro, TRow>(ISqlConnectionFactory conn
         return conexao.Execute(sqlQuery, new { Id = id });
     }
 
+    protected int Execute(string sqlQuery, object parametros)
+    {
+        using SqlConnection conexao = connectionFactory.CreateConnection();
+        conexao.Open();
+
+        return conexao.Execute(sqlQuery, parametros);
+    }
+
     protected IEnumerable<TRegistro> Query(string sqlQuery, Func<TRegistro, bool>? filtro = null)
     {
         using SqlConnection conexao = connectionFactory.CreateConnection();
@@ -36,6 +44,13 @@ public abstract class RepositorioSql<TRegistro, TRow>(ISqlConnectionFactory conn
         using SqlConnection conexao = connectionFactory.CreateConnection();
         conexao.Open();
         return conexao.Query<T>(sqlQuery, new { Id = id }).ToList(); ;
+    }
+
+    protected IEnumerable<T> Query<T>(string sqlQuery, object parametros)
+    {
+        using SqlConnection conexao = connectionFactory.CreateConnection();
+        conexao.Open();
+        return conexao.Query<T>(sqlQuery, parametros).ToList();
     }
 
     protected TRegistro? QuerySingle(string sqlQuery, Guid id)
