c9144f2700367e58b6b4c443b051603db3e28d61
Michel <michelgabiatti07@gmail.com>
2026-06-29 21:37:56 -0300

    Implementacao Dominio

---
+++ eAgenda.WebApp/ModuloContato/Dominio/Contato.cs
@@ -0,0 +1,31 @@
+using eAgenda.WebApp.Compartilhado.ModuloBase;
+
+namespace eAgenda.WebApp.ModuloContato.Dominio;
+
+public class Contato : EntidadeBase<Contato>
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public string? Cargo { get; set; } = null;
+    public string? Empresa { get; set; } = null;
+
+    public Contato() { }
+
+    public Contato(string nome, string email, string telefone, string? cargo = null, string? empresa = null)
+    {
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+        Cargo = cargo;
+        Empresa = empresa;
+    }
+    public override void Atualizar(Contato entidadeAtualizada)
+    {
+        Nome = entidadeAtualizada.Nome;
+        Email = entidadeAtualizada.Email;
+        Telefone = entidadeAtualizada.Telefone;
+        Cargo = entidadeAtualizada.Cargo;
+        Empresa = entidadeAtualizada.Empresa;
+    }
+}
---
+++ eAgenda.WebApp/ModuloContato/Dominio/IRepositorioContato.cs
@@ -0,0 +1,5 @@
+using eAgenda.WebApp.Compartilhado.ModuloBase;
+
+namespace eAgenda.WebApp.ModuloContato.Dominio;
+
+public interface IRepositorioContato : IRepositorio<Contato> { }
